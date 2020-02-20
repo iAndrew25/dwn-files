47092e7ee7f59194b4623375763bb6503c31f5a7
Andrew <tutoit@gmail.com>
2020-02-20 21:11:01 +0200

    Added requirements

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # DWN Files
 Download all the files with a certain extension, from an URL.
 
+## Install:
+```
+$ pip install -r requirements.txt
+```
 ## Usage:
 ```
 $ python dwn-files.py -u [URL] -f [FILE_EXTENSION]
--- dwn-files.py
+++ dwn-files.py
@@ -1,5 +1,4 @@
-import urllib;
-import urlparse
+import urllib
 import mechanize
 import argparse
 import os
@@ -10,13 +9,13 @@ parser.add_argument("-u", "--url", required=True, help="URL of the page you want
 parser.add_argument("-f", "--file", required=True, help="File extension to download.")
 args = parser.parse_args()
 
-print "        _                   __ _ _            "
-print "       | |                 / _(_) |           "
-print "     __| |_      ___ __   | |_ _| | ___  ___  "
-print "    / _` \ \ /\ / / '_ \  |  _| | |/ _ \/ __| "
-print "   | (_| |\ V  V /| | | | | | | | |  __/\__ \ "
-print "    \__,_| \_/\_/ |_| |_| |_| |_|_|\___||___/ "
-print "                                              "
+print("        _                   __ _ _            ")
+print("       | |                 / _(_) |           ")
+print("     __| |_      ___ __   | |_ _| | ___  ___  ")
+print("    / _` \ \ /\ / / '_ \  |  _| | |/ _ \/ __| ")
+print("   | (_| |\ V  V /| | | | | | | | |  __/\__ \ ")
+print("    \__,_| \_/\_/ |_| |_| |_| |_|_|\___||___/ ")
+print("                                              ")
 
 br = mechanize.Browser()
 br.set_handle_robots(False)
@@ -33,16 +32,16 @@ if not os.path.exists(extension):
 br.open(args.url)
 
 for link in br.links():
-	newurl = urlparse.urljoin(link.base_url, link.url)
+	newurl = urllib.parse.urljoin(link.base_url, link.url)
 	if newurl.endswith(extension) and newurl not in visited:
 		count += 1
 		visited.append(newurl)
-		file = urllib.urlopen(newurl)
+		file = urllib.request.urlopen(newurl)
 		size = size + int(file.info().getheaders("Content-Length")[0])
 		output = open(extension + '/' + str(count) + '_' + link.text, 'wb')
 		output.write(file.read())
 		output.close()
-		sys.stdout.writelines(link.text.encode('utf-8') + " downloaded.\n")
+		sys.stdout.writelines(link.text + " downloaded.\n")
 		sys.stdout.flush()
 
-print "\nDONE! " + str(count) + " files (" + str(float(size / 1024 / 1024)) + "MB) have been successfully downloaded."
+print("\nDONE! " + str(count) + " files (" + str(float(size / 1024 / 1024)) + "MB) have been successfully downloaded.")
